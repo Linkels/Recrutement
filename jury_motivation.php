620b7fc2c8408a2eae4f5fbb4a0a8ba50c913f9e
Linkels <m.chaumoitre@simplon-charleville.fr>
2019-05-27 15:48:03 +0200

    Jury fixed

--- jury_motivation.php
+++ jury_motivation.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/jury_tech.css">
+  <link rel="stylesheet" href="css/jury.css">
+  <link rel="stylesheet" href="css/footer.css">
   <title>JURY MOTIVATION</title>
 </head>
 
@@ -85,7 +86,6 @@
 
 
 
-  <?php include "footer.php" ?>
 </body>
 
 </html>
